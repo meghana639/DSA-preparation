b845bc893582c5f8eed8577ed713f10271c6315d
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 09:27:05 +0530

    Implement function to find divisors of a number

--- Basic Maths/print all divisors.java
+++ Basic Maths/print all divisors.java
@@ -1,1 +1,9 @@
+//Divisors of a number:
+class Solution:
+    def divisors(self, n):
+        result = []
+        for i in range(1,n+1):
+            if n % i == 0:
+                result.append(i)
+        return result 
 
