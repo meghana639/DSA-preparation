63f3d1d1c5cf55441b9b1dc5e02e5f927076228f
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 12:28:27 +0530

    Implement max profit calculation for stock trading

--- Arrays/medium/stock buy and sell.java
+++ Arrays/medium/stock buy and sell.java
@@ -1,1 +1,22 @@
+//leetcode: 121 (best time to buy and sell stock):
+class Main {
+    public static int maxprofit(int[] prices){
+        int n = prices.length;
+        int max_profit = 0;
+        int min_price = prices[0];
+        for(int i=1;i<n;i++){
+            if(prices[i] < min_price){
+                min_price = prices[i];
+            }
+            int profit = prices[i] - min_price;
+            max_profit = Math.max(profit,max_profit);
+        }
+        return max_profit;
+    }
+    public static void main(String[] args){
+        int [] prices = {7,1,5,3,6,4};
+        int ans = maxprofit(prices);
+        System.out.print(ans);
+    }
+}
 
