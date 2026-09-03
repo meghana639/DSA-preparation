a420e4684be39adc7c134aefc06624c43d14a606
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 12:27:53 +0530

    Update kadane algorithm.java

--- Arrays/medium/kadane algorithm.java
+++ Arrays/medium/kadane algorithm.java
@@ -1,1 +1,25 @@
+//leetcode: 53 (Maximum subarray):
+class Main {
+    public static int findmaxarray(int[] nums){
+        int n = nums.length;
+        int current_sum = nums[0];
+        int max_sum = nums[0];
+        for(int i=1;i<n;i++){
+            if (current_sum + nums[i] > nums[i]) {
+                current_sum += nums[i];
+                max_sum = Math.max(current_sum,max_sum);
+            }
+            else{
+                current_sum = nums[i];
+                max_sum = Math.max(current_sum,max_sum);
+            }
+        }
+        return max_sum;
+    }
+    public static void main(String[] args){
+        int [] nums = {5,4,-1,7,8};
+        int ans = findmaxarray(nums);
+        System.out.print(ans); 
+    }
+}
 
