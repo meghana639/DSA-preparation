8a802bce10d27e9581645456b879c78f7b549aae
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 09:36:34 +0530

    Update reverse an array.java

--- basic recursion/reverse an array.java
+++ basic recursion/reverse an array.java
@@ -1,1 +1,18 @@
+//reverse an array inplace:
+class Solution:
+    def reverse(self, arr: list, n: int) -> None:
+        left = 0
+        right = n-1
+        while(left < right):
+            arr[left],arr[right] = arr[right],arr[left]
+            left += 1
+            right -= 1
+
+/**/ reverse using another list:
+class Solution:
+    def reverse(self, arr: list, n: int) -> None:  
+        result = []
+        for i in range(n-1,-1,-1) :
+            result.append(arr[i])
+        return result     
 
