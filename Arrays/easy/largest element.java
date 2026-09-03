f4e44c23d891d809dc85a634b8e2ca9e509413e7
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 14:24:14 +0530

    Update largest element.java

--- Arrays/easy/largest element.java
+++ Arrays/easy/largest element.java
@@ -1,1 +1,19 @@
+//Largest element in the array:
+class arrays{
+    public static int largestElement(int[] nums){
+        int n = nums.length;
+        int largest = nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i] > largest){
+                largest = nums[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String[]args){
+        int[] nums = {10,2,5,6,7};
+        int ans = largestElement(nums);
+        System.out.print("the largest is: "+ ans);
+    }
+}
 
