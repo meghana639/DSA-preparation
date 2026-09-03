fe2724c175a10b1653d27be465eb35c077a22aee
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 09:36:46 +0530

    Implement Fibonacci series using recursion and iteration

--- basic recursion/fibonacci series.java
+++ basic recursion/fibonacci series.java
@@ -1,1 +1,30 @@
+//leetcode: 509 (Fibonacci number):
+class Solution:
+    def fib(self, n: int) -> int:
+        first = 0
+        second = 1
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        while(n > 1):
+            third = first + second
+            first = second
+            second = third
+            n -= 1
+        return third 
+//alternate:
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            return self.fib(n-1) + self.fib(n-2)                                           
+
+
+
+
+
 
