98d1ff1a127c1d456efa7759515a4a16a09d830a
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 14:40:39 +0530

    Update Longest subarray with sum K.java

--- Arrays/easy/Longest subarray with sum K.java
+++ Arrays/easy/Longest subarray with sum K.java
@@ -1,1 +1,27 @@
+//Longest subarray with sum K:
+class Main {
+    public static int longestsubarray(int[] nums, int k){
+        int n = nums.length;
+        int max_length = 0;
+        for(int i=0;i<n;i++){
+            int current_sum = 0;
+            for(int j=i;j<n;j++){
+                current_sum += nums[j];
+                if(current_sum == k){
+                    max_length = Math.max(max_length,j-i+1);
+                }
+            }
+        }
+        return max_length;
+    }
+    public static void main(String[]args){
+        int [] nums = {10, 5, 2, 7, 1, 9};
+        int k = 15;
+        int ans = longestsubarray(nums, k);
+        System.out.print(ans);
+    }
+}
+
+
+
 
