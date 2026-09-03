9e22770a03200759b9eb024e88ab82b08b2693ce
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 12:26:33 +0530

    Implement majority element using HashMap
    Added a better solution using HashMap for majority element.

--- Arrays/medium/majority element I.java
+++ Arrays/medium/majority element I.java
@@ -1,1 +1,42 @@
+//leetcode: 169 (majority element):
+//brute force solution:
+class Main{
+    public static int majorityelement(int[] nums){
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            int count = 0;
+            for(int j=0;j<n;j++){
+                if(nums[i] == nums[j]){
+                    count ++;
+                }
+            }
+            if(count > n/2){
+                return nums[i];
+            }
+        }
+        return nums[0];
+    }
+    public static void main(String[]args){
+        int [] nums = {3,2,3};
+        int ans = majorityelement(nums);
+        System.out.print(ans);
+    }
+}
+
+//better solution:
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            int old_frequency = map.getOrDefault(nums[i],0);
+            int new_frequency = old_frequency + 1;
+            map.put(nums[i], new_frequency);
+            if(new_frequency > n/2){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
 
