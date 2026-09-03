f649867a1aa3edfb39b128d9a03677aa414206d7
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 14:39:43 +0530

    Update find missing number.java

--- Arrays/easy/find missing number.java
+++ Arrays/easy/find missing number.java
@@ -1,1 +1,25 @@
+//find the missing number:
+class Main{
+    public static int missingnumber(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<=n;i++){
+            boolean found = false;
+            for(int j=0;j<n;j++){
+                if(nums[j] == i){
+                    found = true;
+                    break;
+                }
+            }
+            if(!found){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[]args){
+        int[] nums = {0,1,2,4};
+        int ans = missingnumber(nums);
+        System.out.print(ans);
+    }
+}
 
