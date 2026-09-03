844465e125df50b1ccb96480967bc7db5bf656a6
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 09:25:17 +0530

    Update palindrome number.java

--- Basic Maths/palindrome number.java
+++ Basic Maths/palindrome number.java
@@ -1,1 +1,15 @@
+//leetcode : 9 (Palindrome Number):
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        count = 0
+        original = x
+        if x < 0:
+            return False
+        while(x>0):
+            num = x % 10
+            count = count * 10 + num
+            x //= 10
+        if count == original:
+            return True  
+        return False        
 
