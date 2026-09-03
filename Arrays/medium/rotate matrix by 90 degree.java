67674a9b829b54b5989a8804b06715a5c7c00a4f
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 12:31:28 +0530

    Update rotate matrix by 90 degree.java

--- Arrays/medium/rotate matrix by 90 degree.java
+++ Arrays/medium/rotate matrix by 90 degree.java
@@ -1,1 +1,37 @@
+//leetcode: 48 (Rotate Image):
+import java.util.*;
+class Main{
+    public static void rotate(int[][] matrix){
+        int n = matrix.length;
+        int m = matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<m;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int left = 0;
+            int right = n-1;
+            while(left < right){
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left ++;
+                right --;
+            }
+        }
+    }
+    public static void main(String[] args){
+        int [][] matrix = {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        rotate(matrix);
+        System.out.println(Arrays.deepToString(matrix));
+    }
+}
+
+
+
+
+
 
