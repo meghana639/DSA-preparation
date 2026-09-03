c26a937dbea7c2f00108cb1a0d223abb6f02cf3f
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 14:29:11 +0530

    Update remove duplicates from sorted array.java

--- Arrays/easy/remove duplicates from sorted array.java
+++ Arrays/easy/remove duplicates from sorted array.java
@@ -1,1 +1,23 @@
+//leetcode: 26 (remove duplicates):
+class arrays{
+    public static int removeduplicates(int[] nums){
+        int i = 0;
+        int n = nums.length;
+        for(int j=1;j<n;j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    } 
+    public static void main(String[] args){
+        int [] nums = {1,2,3,3,4,4,4,5};
+        int ans = removeduplicates(nums);
+        System.out.println(ans);
+        for(int i=0;i<ans;i++){
+            System.out.print(" " + nums[i]);
+        }
+    }
+}
 
