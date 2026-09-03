d6689e331ac896e6b2f93584cb43d072abc08839
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 09:24:52 +0530

    Update count all digits of a number.java

--- Basic Maths/count all digits of a number.java
+++ Basic Maths/count all digits of a number.java
@@ -1,1 +1,11 @@
-
+//Count all Digits of a Number:
+class Solution:
+    def countDigit(self, n):
+        if n == 0:
+            return 1
+        count = 0
+        while(n != 0):
+            n = n // 10
+            count += 1
+        return count
+              
