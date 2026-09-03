c8f2715a8dded48a12af7f8ef871b950d6f99c88
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 09:25:49 +0530

    Implement reverse function for integer input

--- Basic Maths/reverse a number.java
+++ Basic Maths/reverse a number.java
@@ -1,1 +1,16 @@
+//leetcode: 7 (Reverse problem):
+class Solution:
+    def reverse(self, x: int) -> int:
+        count = 0
+        negative = x<0
+        x=abs(x)
+        while(x!=0):
+            num = x%10
+            count = count*10 + num
+            x //= 10 
+        if negative :
+            count = -count
+        if count < -2**31 or count > 2**31 - 1:
+            return 0
+        return count            
 
