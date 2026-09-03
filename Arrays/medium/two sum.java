213de425ddf3d05fa493403815fa8454a1c627be
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 12:24:31 +0530

    Update two sum.java

--- Arrays/medium/two sum.java
+++ Arrays/medium/two sum.java
@@ -1,1 +1,34 @@
+//leetcode: 1 (two sum):
+import java.util.*;
+
+class Main {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int required = target - nums[i];
+
+            if (map.containsKey(required)) {
+                return new int[]{map.get(required), i};
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 2, 4};
+        int target = 6;
+
+        int[] ans = twoSum(nums, target);
+
+        System.out.println(Arrays.toString(ans));
+    }
+}
 
