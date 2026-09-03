d8cdd2c9db441336cb0a3bb500531edc7ce877ec
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 14:38:32 +0530

    Update linear search.java

--- Arrays/easy/linear search.java
+++ Arrays/easy/linear search.java
@@ -1,1 +1,19 @@
+//linear search:
+class Main {
+    public static int linearsearch(int[] nums,int target) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        int [] nums = {2,3,4,5,6,7};
+        int target = 3;
+        int ans = linearsearch(nums, target);
+        System.out.print(" "+ans);
+    }
+}
 
