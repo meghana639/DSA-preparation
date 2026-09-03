6d7186ce8a204ba8d78f86c4d8cc799d80aea375
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 14:40:24 +0530

    Update single number.java

--- Arrays/easy/single number.java
+++ Arrays/easy/single number.java
@@ -1,1 +1,17 @@
+//single number:
+class Main {
+    public static int singlenumber(int[] nums){
+        int n = nums.length;
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans = ans ^ nums[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args){
+        int []nums = {2,2,1};
+        int sol = singlenumber(nums);
+        System.out.print(sol);
+    }
+}
 
