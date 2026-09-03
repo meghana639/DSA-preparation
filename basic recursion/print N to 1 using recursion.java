3aa6e24df50578ab88b904cb72d7f09705413698
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 09:35:41 +0530

    Update print N to 1 using recursion.java

--- basic recursion/print N to 1 using recursion.java
+++ basic recursion/print N to 1 using recursion.java
@@ -1,1 +1,9 @@
+// Print numbers from N to 1 without using loops:
+class Solution:
+    def printNumbers(self, n):
+        if n == 1:
+            print(1)
+        else:
+            print(n)   
+            self.printNumbers(n-1) 
 
