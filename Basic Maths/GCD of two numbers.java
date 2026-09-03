35143fba5274530f4e82ac44c3a449b93931ca8b
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 09:26:45 +0530

    Update GCD of two numbers.java

--- Basic Maths/GCD of two numbers.java
+++ Basic Maths/GCD of two numbers.java
@@ -1,1 +1,27 @@
+// GCD of two numbers:
+class Solution:
+    def GCD(self, n1, n2):
+        gcd = 1
+        for i in range(1,min(n1,n2)+1):
+            if(n1 % i == 0 and n2 % i == 0):
+                gcd = i
+        return gcd   
+
+(alternative solution):
+class Solution:
+    def GCD(self, n1, n2):
+        result1 = []
+        result2 = []
+        for i in range(1,n1+1):
+            if n1 % i == 0:
+                result1.append(i)
+        for j in range(1,n2+1):
+            if n2 % i == 0:
+                result2.append(j)
+
+        gcd = 1
+        for x in result1:
+            if x in result2:
+                gcd = x
+        return gcd        
 
