aa9d1d0f0a08078f4fae220acf8d376c69d34d08
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 09:27:29 +0530

    Update check for prime numbers.java

--- Basic Maths/check for prime numbers.java
+++ Basic Maths/check for prime numbers.java
@@ -1,1 +1,11 @@
+// Prime Numbers:
+class Solution:
+    def isPrime(self, n):
+        result = []
+        for i in range(1,n+1):
+            if n % i == 0:
+                result.append(i)
+        if len(result) == 2:
+            return True
+        return False               
 
