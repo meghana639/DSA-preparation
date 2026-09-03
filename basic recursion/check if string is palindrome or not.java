2ccfa6b3a6d6704df70b4b14e66de18a48c6949f
Gurijala Meghana <meghana7275@gmail.com>
2026-09-03 09:34:52 +0530

    Update check if string is palindrome or not.java

--- basic recursion/check if string is palindrome or not.java
+++ basic recursion/check if string is palindrome or not.java
@@ -1,1 +1,12 @@
+//leetcode: 125 (Valid Palindrome):
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        phrase = ""
+        s = s.lower()
+        for i in range(len(s)):
+            if s[i].isalnum():
+                phrase += s[i]      
+        if phrase == phrase[::-1]:
+            return True
+        return False       
 
